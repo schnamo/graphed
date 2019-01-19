353da08d9c65eb0d996fb8df815a9f6818cb8aea
charlotte tumescheit <lotti@charlottes-MacBook-Pro.local>
2019-01-19 16:36:32 +0000

    get workspace, create note

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import request, Flask, render_template, Markup, redirect, jsonify
 from .database import engine, User, Token, Workspace, Note, Connection
+from .error import MissingInformation, InvalidInformation
 import sqlalchemy as db
 from sqlalchemy import sql
 
@@ -32,7 +33,37 @@ def create_workspace(name):
 # Return all nodes in workspace (list of notes)
 @app.route("/workspace/<id>")
 def get_workspace(id):
-    pass
+    token = request.cookies.get("token") # get token
+    owner = cookie_to_user(token)
+
+    conn = engine.connect()
+
+    notes_query = sql.select([Note.__table__])\ # define query for db request to get all nodes for workspace id
+        .where(Note.workspace == id)
+    connections_query = sql.select([Note.__table__])\ # define query for db request to get all nodes for workspace id
+        .where(Note.workspace == id)
+
+    notes = conn.execute(notes_query).fetchall()
+    connections = conn.execute(connections_query).fetchall()
+
+    workspace_notes = []
+    for note in notes: # build nodes for json
+        workspace_notes.append({
+        "node" : note.id,
+        "name" : note.name,
+        })
+    workspace_connections = []
+    for connection in connections:
+        workspace_connections.append({
+        "origin" : connection.origin,
+        "target" : connection.target,
+        })
+
+    return jsonify({
+            "notes" : workspace_notes,
+            "connections" : workspace_connections
+        })
+
 
 # Create new workspace and return id
 @app.route("/workspace/delete/<id>")
@@ -44,15 +75,29 @@ def delete_workspace(id):
 def get_token():
     pass
 
-# Create user 
+# Create user
 @app.route("/register", methods=['POST'])
 def register():
     pass
 
 # Create note in workspace
-@app.route("/workspace/<id>/create/")
-def create_note(id):
-    pass
+@app.route("/workspace/<id>/create/<name>")
+def create_note(id, name):
+    token = request.cookies.get("token") # get token
+    owner = cookie_to_user(token)
+
+    conn = engine.connect()
+    query = sql.insert(Note.__table__,
+            values={
+                Note.name: name,
+                Workspace.owner: owner
+                }
+            )
+    result = conn.execute(query)
+    return jsonify({
+            "status": "ok",
+            "id": result.lastrowid,
+        })
 
 # Connect two nodes
 @app.route("/workspace/<id>/connect/<origin>/<target>")
